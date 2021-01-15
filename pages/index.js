12d829c982a04a460f46aa2888b6c5f9d4826378
jdhofmann <justindhofmann@gmail.com>
2021-01-14 21:37:24 -0500

    added color boxes

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,12 @@ export default function Home() {
   return (
     <div>
       <Header />
-        <h1 className={styles.title}>
+        <h1>
           Welcome</h1>
+          <div className="container ct-0 st-0"></div>
+          <div className="container ct-1 st-1"></div>
+          <div className="container ct-2 st-2"></div>
+          <div className="container ct-3 st-3"></div>
     </div>
   )
 }
